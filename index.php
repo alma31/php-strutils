c01395f8d3970e7b00d76bca4cf89cdc689b1e55
alma31 <lenni31@neuf.fr>
2017-01-16 13:59:42 +0100

    exercise pas fini

--- index.php
+++ index.php
@@ -7,10 +7,48 @@
 <body>
 	<?php 
 
+	class StrUtils{
+		private $str = "ma chaine de carractere";
+		
+		function __construct(){
+			$this->str;
+		}
+
+		public function getStr(){
+			return $this->str;
+		}
+		public function bold(){
+			return '<b>'.$this->str.'</b></br>';
+			$this->str = $bold;
+		}
+
+		public function italic(){
+			return '<em>'.$this->str.'</em></br>';
+			$this->str = $italic;
+		}
+
+		public function underline(){
+
+		}
+
+		public function capitalize(){
+
+		}
+
+		public function uglify(){
+			$this->bold();
+			$this->italic();		
+		}
+	}
+
+	$str = new StrUtils();
+	$str->uglify();
+
+	echo  $str->uglify();
 
 
 
-	 ?>
+	?>
 	
 </body>
 </html>
